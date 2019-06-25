48a9fbfcea25c19e06e42bbbfa76b0c6b55c77f2
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-25 18:38:44 +0530

    changed variable types of numbers

--- variables.tf
+++ variables.tf
@@ -7,13 +7,13 @@ variable "delivery_frequency" {
 variable "enable_recorder" {
   default     = true
   description = "Whether the configuration recorder should be enabled or disabled."
-  type        = string
+  type        = number
 }
 
 variable "expiration" {
   default     = 2555
   description = "The number of days to wait before expiring an object"
-  type        = string
+  type        = number
 }
 
 variable "log_bucket" {
@@ -63,7 +63,7 @@ variable "rules" {
 variable "rules_count" {
   default = 8
   description = "The count of the items in the rules list"
-  type = string
+  type = number
 }
 
 variable "scopes" {
@@ -153,6 +153,6 @@ variable "tags" {
 variable "transition_to_glacier" {
   default = 30
   description = "The number of days to wait before transitioning an object to Glacier"
-  type = string
+  type = number
 }
 
