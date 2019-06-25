8595fcb21979a97f488ed52c5d1e79a4dfb2e3bf
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-25 17:07:43 +0530

    terraform 012 upgrade

--- main.tf
+++ main.tf
@@ -1,87 +1,83 @@
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = [
-      "sts:AssumeRole"
+      "sts:AssumeRole",
     ]
     principals {
       identifiers = [
-        "config.amazonaws.com"
+        "config.amazonaws.com",
       ]
-      type  = "Service"
+      type = "Service"
     }
   }
 }
 
 resource "aws_iam_role" "config_role" {
-  assume_role_policy  = "${data.aws_iam_policy_document.assume_role_policy.json}"
-  name                = "${local.config_name}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name               = local.config_name
 }
 
 data "aws_iam_policy_document" "role_policy" {
   statement {
     actions = [
-      "s3:PutObject"
+      "s3:PutObject",
     ]
     resources = [
-      "${aws_s3_bucket.config.arn}",
-      "${aws_s3_bucket.config.arn}/*"
+      aws_s3_bucket.config.arn,
+      "${aws_s3_bucket.config.arn}/*",
     ]
   }
 }
 
 resource "aws_iam_role_policy" "config" {
-  name    = "config-bucket-access"
-  policy  = "${data.aws_iam_policy_document.role_policy.json}"
-  role    = "${aws_iam_role.config_role.id}"
+  name   = "config-bucket-access"
+  policy = data.aws_iam_policy_document.role_policy.json
+  role   = aws_iam_role.config_role.id
 }
 
 resource "aws_iam_role_policy_attachment" "config" {
-  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
-  role        = "${aws_iam_role.config_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+  role       = aws_iam_role.config_role.name
 }
 
 resource "aws_config_configuration_recorder" "config" {
-  name      = "${var.name}"
+  name = var.name
   recording_group {
     include_global_resource_types = true
   }
-  role_arn  = "${aws_iam_role.config_role.arn}"
+  role_arn = aws_iam_role.config_role.arn
 }
 
 resource "aws_sns_topic" "config" {
-  name = "${local.config_name}"
+  name = local.config_name
 }
 
 resource "aws_config_delivery_channel" "config" {
-  depends_on      = [
-    "aws_config_configuration_recorder.config"
-  ]
-  name            = "${var.name}"
-  s3_bucket_name  = "${aws_s3_bucket.config.id}"
+  depends_on     = [aws_config_configuration_recorder.config]
+  name           = var.name
+  s3_bucket_name = aws_s3_bucket.config.id
   snapshot_delivery_properties {
-    delivery_frequency = "${var.delivery_frequency}"
+    delivery_frequency = var.delivery_frequency
   }
-  sns_topic_arn   = "${aws_sns_topic.config.arn}"
+  sns_topic_arn = aws_sns_topic.config.arn
 }
 
 resource "aws_config_configuration_recorder_status" "config" {
-  depends_on  = [
-    "aws_config_delivery_channel.config"
-  ]
-  is_enabled  = "${var.enable_recorder}"
-  name        = "${aws_config_configuration_recorder.config.name}"
+  depends_on = [aws_config_delivery_channel.config]
+  is_enabled = var.enable_recorder
+  name       = aws_config_configuration_recorder.config.name
 }
 
 resource "aws_config_config_rule" "rule" {
-  count             = "${var.rules_count}"
-  depends_on        = [
-    "aws_config_configuration_recorder.config"
-  ]
-  input_parameters  = "${lookup(var.input_parameters, element(var.rules, count.index), "")}"
-  name              = "${element(var.rules, count.index)}"
-#  scope             = ["${local.scopes[contains(keys(local.scopes), element(var.rules, count.index)) ? element(var.rules, count.index) : local.default_scope_key]}"]
+  count            = var.rules_count
+  depends_on       = [aws_config_configuration_recorder.config]
+  input_parameters = lookup(var.input_parameters, element(var.rules, count.index), "")
+  name             = element(var.rules, count.index)
+
+  #  scope             = ["${local.scopes[contains(keys(local.scopes), element(var.rules, count.index)) ? element(var.rules, count.index) : local.default_scope_key]}"]
   source {
     owner             = "AWS"
-    source_identifier = "${lookup(local.source_identifiers, element(var.rules, count.index))}"
+    source_identifier = local.source_identifiers[element(var.rules, count.index)]
   }
 }
+
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,61 +1,61 @@
 resource "aws_s3_bucket" "config" {
-  acl     = "log-delivery-write"
-  bucket  = "${local.config_name}"
+  acl    = "log-delivery-write"
+  bucket = local.config_name
 
   lifecycle {
     prevent_destroy = true
   }
 
   lifecycle_rule {
-    id = "log"
+    id      = "log"
     enabled = true
 
     transition {
-      days          = "${var.transition_to_glacier}"
+      days          = var.transition_to_glacier
       storage_class = "GLACIER"
     }
 
     expiration {
-      days = "${var.expiration}"
+      days = var.expiration
     }
   }
 
   logging {
-    target_bucket = "${var.log_bucket}"
+    target_bucket = var.log_bucket
     target_prefix = "s3/${local.config_name}/"
   }
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 data "aws_iam_policy_document" "config" {
-
   statement {
     actions = [
-      "s3:*"
+      "s3:*",
     ]
     condition {
       test = "Bool"
       values = [
-        "false"
+        "false",
       ]
       variable = "aws:SecureTransport"
     }
     effect = "Deny"
     principals {
       identifiers = [
-        "*"
+        "*",
       ]
       type = "AWS"
     }
     resources = [
-      "${aws_s3_bucket.config.arn}",
-      "${aws_s3_bucket.config.arn}/*"
+      aws_s3_bucket.config.arn,
+      "${aws_s3_bucket.config.arn}/*",
     ]
     sid = "DenyUnsecuredTransport"
   }
 }
 
 resource "aws_s3_bucket_policy" "config" {
-  bucket = "${aws_s3_bucket.config.id}"
-  policy = "${data.aws_iam_policy_document.config.json}"
+  bucket = aws_s3_bucket.config.id
+  policy = data.aws_iam_policy_document.config.json
 }
+
