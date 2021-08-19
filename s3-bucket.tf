c2d583913536734e335a9660d3da733d041bd4f6
Siddalingesh <sangadi@quinovas.com>
2021-08-19 15:25:06 +0000

    Added aws_s3_bucket_public_access_block resource

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -59,3 +59,10 @@ resource "aws_s3_bucket_policy" "config" {
   policy = data.aws_iam_policy_document.config.json
 }
 
+resource "aws_s3_bucket_public_access_block" "config" {
+  bucket                  = aws_s3_bucket.config.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
